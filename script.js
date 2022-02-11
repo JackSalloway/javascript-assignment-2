f1ae7d8cd2af4f144df9c3745477d8fa4158a721
Jack Salloway <jacksalloway@hotmail.com>
2022-02-11 11:15:48 +0000

    Add dynamic id to each grid box
    Add query selector for all grid boxes

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // identifers
 const container = document.querySelector('.sketch-container');
+const gridBox = document.querySelectorAll('.grid');
 
 // create gridCount variable
 let gridCount = 0;
 
 for (gridCount; gridCount <= 255; gridCount++) {
     const divGrid = document.createElement('div');
-    divGrid.classList.add(`grid-${gridCount}`);
+    divGrid.classList.add('grid');
+    divGrid.setAttribute('id', `${gridCount + 1}`)
     divGrid.textContent = `${gridCount + 1}`
 
     container.appendChild(divGrid);
